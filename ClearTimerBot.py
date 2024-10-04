e0f1aaa98f10fcb6b1ca60adcc7a5f0c114b06f5
biast12 <tobiaspoulsen@hotmail.com>
2024-10-04 20:31:03 +0200

    Suppress all discord logs (removes double logs)

--- ClearTimerBot.py
+++ ClearTimerBot.py
@@ -19,6 +19,18 @@ from apscheduler.schedulers.asyncio import AsyncIOScheduler
 logging.basicConfig(level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S', format='[%(asctime)s] [%(levelname)s] %(message)s')
 logger = logging.getLogger(__name__)
 
+# Suppress all discord logs
+discord_loggers = [
+    'discord',
+    'discord.gateway',
+    'discord.client',
+    'discord.ext.commands',
+    'discord.ext.commands.bot',
+]
+
+for logger_name in discord_loggers:
+    logging.getLogger(logger_name).setLevel(logging.CRITICAL)
+
 # Intents for the bot
 intents = discord.Intents.default()
 intents.messages = True  # Ensure message intents are enabled
